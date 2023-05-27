0de9ba447c6686ad3e0a8a27b9ffeb1521d8fce5
Uğur Eren <contact@ugureren.net>
2023-05-27 22:17:11 +0300

    Party increment action

--- src/Screens/Landing/Party/Party.styles.ts
+++ src/Screens/Landing/Party/Party.styles.ts
@@ -4,9 +4,13 @@ import {Theme} from '../../../Styles';
 export default StyleSheet.create({
   container: {
     flex: 1,
+  },
+  touchable: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
+
   rte: {
     backgroundColor: Theme.colors.akp,
   },
--- src/Screens/Landing/Party/Party.tsx
+++ src/Screens/Landing/Party/Party.tsx
@@ -1,18 +1,26 @@
+import {memo} from 'react';
 import {View} from 'react-native';
-import {Text} from '../../../Components';
+import {VotesActions, useAppDispatch} from '../../../Redux';
+import {Text, Touchable} from '../../../Components';
 import {PartyProps} from './Party.props';
 import styles from './Party.styles';
 
 const Party: React.FC<PartyProps> = (props) => {
   const {party} = props;
 
+  const dispath = useAppDispatch();
+
+  const onPress = () => dispath(VotesActions.increment(party));
+
   return (
     <View style={[styles.container, styles[party]]}>
-      <Text weight="bold" color="textStrong" fontSize={32}>
-        {party === 'rte' ? ':(' : 'Kılıçdaroğlu'}
-      </Text>
+      <Touchable style={styles.touchable} onPress={onPress}>
+        <Text weight="bold" color="textStrong" fontSize={32}>
+          {party === 'rte' ? ':(' : 'Kılıçdaroğlu'}
+        </Text>
+      </Touchable>
     </View>
   );
 };
 
-export default Party;
+export default memo(Party);
